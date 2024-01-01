053ad284dbf5c7954d62573b3536a3224b64d9ff
Ashfaq Khan <82702762+ashfaqrafin@users.noreply.github.com>
2024-01-01 22:07:10 +0600

    strings

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Ashfaq";
+        System.out.println(name.charAt(0));
+        System.out.println(name.length());
+        name.replace('q', 'k');
+        System.out.println(name.substring(0, 3));
+
+    }
+    
+}

b46f8a31c854d4e0ac752b8c30fb29cfdfbe39c7
Ashfaq Khan <82702762+ashfaqrafin@users.noreply.github.com>
2024-01-01 21:24:12 +0600

    Add Hello.class and Variables.java

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args){
+        int myNumber = 42;
+        boolean isFun = true;
+        char movieRating = 'A';
+        System.out.println(myNumber);
+        System.out.println(isFun);
+        System.out.println(movieRating);
+    }
+}

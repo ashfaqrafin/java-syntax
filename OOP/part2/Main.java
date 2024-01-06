cfbab306043f3667237151e25012fdcd2fb2e0bd
Ashfaq Khan <82702762+ashfaqrafin@users.noreply.github.com>
2024-01-06 23:03:25 +0600

    waste of time

---
+++ OOP/part2/Main.java
@@ -0,0 +1,9 @@
+package OOP.part2;
+
+import static OOP.part2.Message.message;
+
+public class Main {
+   public static void main(String[] args) {
+      message();
+   }
+}
---
+++ OOP/part2/Message.java
@@ -0,0 +1,11 @@
+package OOP.part2;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+
+    public static void message() {
+        System.out.println("Fuck Yourself");
+    }
+}
